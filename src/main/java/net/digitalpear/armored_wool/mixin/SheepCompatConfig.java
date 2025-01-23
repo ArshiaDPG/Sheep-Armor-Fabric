8b6978334b0e33382cf17ac4f337c2bdac2a38d2
DP <digitalpeargaming@gmail.com>
2025-01-22 16:34:13 -0800

    added mixin stuff

---
+++ src/main/java/net/digitalpear/armored_wool/mixin/SheepCompatConfig.java
@@ -0,0 +1,52 @@
+package net.digitalpear.armored_wool.mixin;
+
+import com.google.common.collect.ImmutableMap;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+
+public class SheepCompatConfig implements IMixinConfigPlugin {
+
+    public static final Map<String, Supplier<Boolean>> COMPAT_CHECKS = ImmutableMap.of(
+//            "net.digitalpear.armored_wool.mixin.EnergeticSheepMixin", () -> FabricLoader.getInstance().isModLoaded("energeticsheep")
+    );
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return COMPAT_CHECKS.getOrDefault(mixinClassName, () -> true).get();
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
